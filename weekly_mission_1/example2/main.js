13b65453b6f8d0201be0f2dce922e3826e657f61
OscarCabralesA <aran.danonoconuva@gmail.com>
2022-04-12 17:40:30 -0500

    add examples 1 to 8

---
+++ weekly_mission_1/example2/main.js
@@ -0,0 +1,9 @@
+// node main.js
+
+//import { hello } from './logger'
+const logger = require('./logger')
+
+
+
+logger.info('This is an informational message')
+logger.verbose('This is a verbose message')
