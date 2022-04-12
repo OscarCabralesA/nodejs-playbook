88dbd36b736124f6824de35a198c9aa4d1559c02
OscarCabralesA <aran.danonoconuva@gmail.com>
2022-04-12 18:00:24 -0500

    add example 10

---
+++ weekly_mission_1/example10/pokemon.js
@@ -0,0 +1,13 @@
+export default class Pokemon {
+    constructor(name) {
+        this.name = name
+    }
+
+    sayMessage(message) {
+        console.log(`Mi pokemon ${this.name} dice: ${message}`)
+    }
+
+    sayHello() {
+        console.log(`Mi pokemon ${this.name} te saluda!!!`)
+    }
+}
