02c17dbecf48f69b1cd5d1d4fb3ca78b3533a29f
OscarCabralesA <aran.danonoconuva@gmail.com>
2022-04-15 18:50:33 -0500

    added uber model

---
+++ weekly_mission_2/exercises/exercise1/uber.js
@@ -0,0 +1,30 @@
+const user = {
+    id: "323e5gt4235",
+    firstName: "Oscar",
+    lastName: "Cabrales" , 
+    picture: "pfp.png",
+    phoneNumber: "+52 3349323518", 
+    email: "oscar.cab@gmail.com", 
+    password: "11w5e2e$", 
+    googleAccount: "Oscar Cabrales Alcalá", 
+    facebookAccount: "Oscar Cab"
+}
+
+const travel = {
+    requester: "323e5gt4235", // user_id 
+    ride: {
+        driver: {
+            id: "d3s1a-121", // user_id
+            name: "Jacob", 
+            stars: 4.85, 
+        },
+        car: {
+            model: "BLACK Toyota Prius", 
+            plates: "P 688 CC", 
+        }
+    },
+    pickupPoint: "683 Calz Independencia Sur Guadalajara, Jalisco", 
+    destination: "171 Independencia Zapopan, Jalisco", 
+    cost: 220,
+    estimatedArrivalTime: 20
+}
