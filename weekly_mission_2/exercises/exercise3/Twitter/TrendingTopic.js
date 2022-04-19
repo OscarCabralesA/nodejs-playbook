7596dcf33fb7a3446e6c344489c0a3c97e39f4de
OscarCabralesA <aran.danonoconuva@gmail.com>
2022-04-19 17:30:01 -0500

    added week 2 exercise3 Twitter/TrendingTopic class

---
+++ weekly_mission_2/exercises/exercise3/Twitter/TrendingTopic.js
@@ -0,0 +1,15 @@
+class TrendingTopic {
+    constructor(hashtag, topic) {
+        this.hashtag = hashtag
+        this.topic = topic
+        this.tweets = 0
+    }
+
+    addTweet() {
+        this.tweets += 1
+    }
+
+    removeTweet() {
+        this.tweets -= 1
+    }
+}
