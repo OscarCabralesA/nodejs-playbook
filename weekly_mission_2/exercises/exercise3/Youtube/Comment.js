d703fd02738bfd11e7b49544670897c83d030601
OscarCabralesA <aran.danonoconuva@gmail.com>
2022-04-19 17:43:54 -0500

    added week 2 exercise3 Youtube/Comment class

---
+++ weekly_mission_2/exercises/exercise3/Youtube/Comment.js
@@ -0,0 +1,15 @@
+class Comment {
+    constructor(channelId, text) {
+        this.channelId = channelId
+        this.text = text
+        this.date = new Date()
+        this.likes = 0
+        this.dislikes = 0
+    }
+    like() {
+        this.likes += 1
+    }
+    dislike() {
+        this.dislikes += 1
+    }
+}
