d6f47e8d032d357d4f8ad38f96656d25bdae7637
OscarCabralesA <aran.danonoconuva@gmail.com>
2022-04-19 17:27:39 -0500

    added week 2 exercise3 Twitter/User class

---
+++ weekly_mission_2/exercises/exercise3/Twitter/User.js
@@ -0,0 +1,50 @@
+class User {
+    constructor (user, username, bio) {
+        this.user = user
+        this.username = username
+        this.bio = bio
+        this.followers = []
+        this.following = []
+        this.tweets = []
+        this.media = []
+        this.likes = []
+    }
+
+    get numberOfFollowers() {
+        return this.followers.length
+    }
+
+    get numberOfFollowing() {
+        return this.following.length
+    }
+
+    get numberOfTweets() {
+        return this.tweets.length
+    }
+
+    addFollower(user_id) {
+        if (!this.followers.includes(user_id)) {
+            this.followers.push(user_id)
+        }
+    }
+    addFollowing(user_id) {
+        if (!this.following.includes(user_id)) {
+            this.following.push(user_id)
+        }
+    }
+    removeFollower(user_id) {
+        var fllwr = this.followers.indexOf(user_id)
+        if (fllwr > -1) {
+            this.followers = this.followers.splice(fllwr, 1)
+        }
+    }
+    removeFollowing(user_id) {
+        var fllwg = this.following.indexOf(user_id)
+        if (fllwg > -1) {
+            this.following = this.following.splice(fllwg, 1)
+        }
+    }
+    tweet(tweet) {
+        this.tweets.push(tweet)
+    }
+}
